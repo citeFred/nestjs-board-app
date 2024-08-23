1f2cc19c1c2b849c012a81e06e5af7c3890d4c7b
citeFred <citefred@gmail.com>
2024-08-23 14:44:05 +0900

    생성자 주입 필드 생성과 초기화 한번에 다시 변경

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -3,11 +3,8 @@ import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
-    // 필드 멤버 선언
-    private boardsService: BoardsService;
-    
     // 생성자 주입(DI)
-    constructor(boardsService: BoardsService){}
+    constructor(private boardsService: BoardsService){}
     
     @Get('/') // GetMapping 핸들러 데코레이터
     getAllBoard(){
