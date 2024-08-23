0a2575020b8110b715184e9098cde229659a0c69
citeFred <citefred@gmail.com>
2024-08-23 14:43:36 +0900

    생성자 주입의 원래 형태

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -3,8 +3,11 @@ import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
+    // 필드 멤버 선언
+    private boardsService: BoardsService;
+    
     // 생성자 주입(DI)
-    constructor(private boardsService: BoardsService){}
+    constructor(boardsService: BoardsService){}
     
     @Get('/') // GetMapping 핸들러 데코레이터
     getAllBoard(){
