5b326a6aecd2ab74d3afa7e48bf24d3c67d7b072
citeFred <citefred@gmail.com>
2024-08-26 23:50:31 +0900

    Custom Repository 주석처리

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -2,21 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsService } from './boards.service';
 import { Board } from './board.entity';
-import { DataSource } from 'typeorm';
-import { BoardsRepository } from './boards.repository'; // 리포지토리 파일의 경로
+// import { BoardsRepository } from './boards.repository'; // 커스텀 리포지토리 파일의 경로
 
 @Module({
   imports: [TypeOrmModule.forFeature([Board])],
-  providers: [
-    BoardsService,
-    
-    // CustomRepository가 필요한 경우 다음처럼 DataSource객체를 활용해야 한다.
-    { 
-      provide: 'BOARDS_REPOSITORY', useFactory: (dataSource: DataSource) => { return new BoardsRepository(dataSource); },
-      inject: [DataSource],
-    },
-
-  ],
+  providers: [ BoardsService ], //   providers: [ BoardsService, BoardsRepository ], // 커스텀 리포지토리 필요한 경우
   exports: [BoardsService],
 })
 export class BoardsModule {}
--- src/boards/boards.repository.ts
+++ src/boards/boards.repository.ts
@@ -1,15 +1,15 @@
-import { DataSource, Repository } from 'typeorm';
-import { Board } from './board.entity';
+// import { EntityRepository, Repository } from 'typeorm';
+// import { Board } from './board.entity';
 
-export class BoardsRepository extends Repository<Board> {
-    constructor(dataSource: DataSource) {
-      super(Board, dataSource.createEntityManager());
-    }
-  
-    // 쿼리 커스텀이 필요한 경우 아래처럼 커스텀 메서드 아래와 같이 추가한다.
-    // async findByAuthor(author: string): Promise<Board[]> {
-    //   return this.createQueryBuilder('board')
-    //     .where('board.author = :author', { author })
-    //     .getMany();
-    // }
-  }
+// @EntityRepository(Board)
+// export class BoardsRepository extends Repository<Board> {
+//  커스텀 메서드 예제
+
+//   async findByAuthor(author: string): Promise<Board[]> {
+//     return this.find({ where: { author } });
+//   }
+
+//   async findOneById(id: number): Promise<Board | undefined> {
+//     return this.findOneBy({ id });
+//   }
+// }
