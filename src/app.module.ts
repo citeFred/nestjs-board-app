ed85b831484b31b768f65405b378e22124e17417
citeFred <citefred@gmail.com>
2024-08-27 18:24:53 +0900

    회원가입및로그인 인증인가를 위한 Auth 모듈 생성 및 User 엔터티 작성 기본 골격

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { BoardsModule } from './boards/boards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeorm.config';
 import { GlobalModule } from './global.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     GlobalModule,
     TypeOrmModule.forRoot(typeOrmConfig),
-    BoardsModule],
+    BoardsModule,
+    AuthModule],
 })
 export class AppModule {}
