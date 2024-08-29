74b096d941a6af07f4b6f9f78ef0b5062537f893
citeFred <citefred@gmail.com>
2024-08-29 22:01:23 +0900

    회원가입기능의 기본 골격 Service+Repository(TypeORM) 구성

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,13 +2,28 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserRole } from './user-role.enum';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(User)
-        private UsersRepository: Repository<User>
+        private usersRepository: Repository<User>
     ){}
 
+    // 회원 가입
+    async createUser(createUserDto: CreateUserDto): Promise<User> {
+        const { username, password, email, role } = createUserDto;
+    
+        const user = this.usersRepository.create({
+          username,
+          password,
+          email,
+          role,
+        });
+    
+        return await this.usersRepository.save(user);
+    }
 }
 
