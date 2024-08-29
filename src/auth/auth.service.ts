d7c70fbdc1ec566ccd7ea79d917036ba57a64877
citeFred <citefred@gmail.com>
2024-08-29 22:27:48 +0900

    회원가입에서 이메일중복검사 로직 추가 checkEmailExists() 메서드 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UserRole } from './user-role.enum';
 
 @Injectable()
 export class AuthService {
@@ -15,15 +14,26 @@ export class AuthService {
     // 회원 가입
     async createUser(createUserDto: CreateUserDto): Promise<User> {
         const { username, password, email, role } = createUserDto;
-    
+
+        // 이메일 중복 확인
+        await this.checkEmailExists(email);
+
         const user = this.usersRepository.create({
-          username,
-          password,
-          email,
-          role,
+            username,
+            password,
+            email,
+            role,
         });
-    
+
         return await this.usersRepository.save(user);
+    }
+
+    // 이메일 중복 확인 메서드
+    private async checkEmailExists(email: string): Promise<void> {
+        const existingUser = await this.usersRepository.findOne({ where: { email } });
+        if (existingUser) {
+            throw new ConflictException('Email already exists');
+        }
     }
 }
 
