0e6190ad17f4594e5f0693f4cd8117c53495b1df
citeFred <citefred@gmail.com>
2024-08-29 22:01:57 +0900

    회원가입기능의 기본 골격 Controller 구성

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
 
-@Controller('auth')
+@Controller('api/auth')
 export class AuthController {
     constructor(private authService: AuthService){}
 
+    // 회원 가입 기능
+    @Post('/signup') // PostMapping 핸들러 데코레이터
+    signup(@Body() createUserDto: CreateUserDto): Promise<User> {
+        return this.authService.createUser(createUserDto);
+    }
 }
