dbe1cc3e6461c879a48c98119d298c7becf9ff9a
citeFred <citefred@gmail.com>
2024-08-30 15:54:56 +0900

    로그인 기능의 기본 골격 Service+Repository(TypeORM) 구성

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,10 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcryptjs';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -13,7 +14,7 @@ export class AuthService {
     ){}
 
     // 회원 가입
-    async signup(createUserDto: CreateUserDto): Promise<User> {
+    async signUp(createUserDto: CreateUserDto): Promise<User> {
         const { username, password, email, role } = createUserDto;
 
         // 이메일 중복 확인
@@ -32,16 +33,30 @@ export class AuthService {
         return await this.usersRepository.save(user);
     }
 
-    async signin() {
-        
+    // 로그인
+    async signIn(loginUserDto: LoginUserDto): Promise<string> {
+        const { email, password } = loginUserDto;
+
+        const existingUser = await this.findUserByEmail(email);
+
+        if (!existingUser || !(await bcrypt.compare(password, existingUser.password))) {
+            throw new UnauthorizedException('Incorrect email or password.');
+        }
+
+        return 'login success';
     }
 
     // 이메일 중복 확인 메서드
     private async checkEmailExists(email: string): Promise<void> {
-        const existingUser = await this.usersRepository.findOne({ where: { email } });
+        const existingUser = await this.findUserByEmail(email);
         if (existingUser) {
             throw new ConflictException('Email already exists');
         }
+    }
+
+    // 이메일로 유저 찾기 메서드
+    private async findUserByEmail(email: string): Promise<User | undefined> {
+        return await this.usersRepository.findOne({ where: { email } });
     }
 
     // 비밀번호 해싱 암호화 메서드
