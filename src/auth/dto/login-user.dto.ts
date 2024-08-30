1407971afc8a9c89a2a0611f592196c5fa191629
citeFred <citefred@gmail.com>
2024-08-30 15:54:07 +0900

    로그인 DTO 유효성검사 축소

--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,13 +1,11 @@
-import { IsEmail, IsNotEmpty, Matches, MaxLength } from "class-validator";
+import { IsNotEmpty, MaxLength } from "class-validator";
 
 export class LoginUserDto {
     @IsNotEmpty()
     @MaxLength(20)
-    @Matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, { message: 'Password too weak', }) // 대문자, 소문자, 숫자, 특수문자 포함
     password: string;
 
     @IsNotEmpty()
-    @IsEmail() // 이메일 형식
     @MaxLength(100)
     email: string;
 }
