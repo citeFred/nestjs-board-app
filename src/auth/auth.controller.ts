b6df1dbed95d95fd6d712347149b0e2450d1e539
citeFred <citefred@gmail.com>
2024-08-31 21:31:28 +0900

    CustomDecorator 생성 및 사용해보기

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,6 +5,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.entity';
 import { LoginUserDto } from './dto/login-user.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './get-user.decorator';
 
 @Controller('api/auth')
 export class AuthController {
@@ -25,8 +26,9 @@ export class AuthController {
     // 인증된 회원이 들어갈 수 있는 테스트 URL 경로
     @Post('/test')
     @UseGuards(AuthGuard()) // @UseGuards : 핸들러는 지정한 인증 가드가 적용됨 -> AuthGuard()의 'jwt'는 기본값으로 생략가능
-    testForAuth(@Req() req: Request) {
-        console.log(req.user); // 인증된 사용자의 정보를 출력
-        return { message: 'You are authenticated', user: req.user };
+    testForAuth(@GetUser() user: User) {
+        console.log(user); // 인증된 사용자의 정보를 출력
+        console.log(user.email); // .연산자로 객체처럼 접근 가능
+        return { message: 'You are authenticated', user: user };
     }
 }
