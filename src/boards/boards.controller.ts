ca1f38a447764982512279bdcf51f0e9511184be
citeFred <citefred@gmail.com>
2024-08-31 22:58:52 +0900

    게시판 인가 구현 적용, 게시판 접근 권한 설정

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,18 +1,24 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query, UseGuards } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.entity';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatus } from './board-status.enum';
 import { UpdateBoardDto } from './dto/update-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/auth/custom-role.guard';
+import { Roles } from 'src/auth/roles.decorator';
+import { UserRole } from 'src/auth/user-role.enum';
 
 @Controller('api/boards')
+@UseGuards(AuthGuard('jwt'), RolesGuard) // JWT 인증과 role 커스텀 가드를 적용
 export class BoardsController {
     // 생성자 주입(DI)
     constructor(private boardsService: BoardsService){}
     
     // 게시글 작성 기능
     @Post('/') // PostMapping 핸들러 데코레이터
+    @Roles(UserRole.USER) // User만 게시글 작성 가능
     createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
         return this.boardsService.createBoard(createBoardDto)
     }
@@ -37,6 +43,7 @@ export class BoardsController {
 
     // 특정 번호의 게시글 삭제
     @Delete('/:id')
+    @Roles(UserRole.ADMIN) // ADMIN만 게시글 삭제 가능
     deleteBoardById(@Param('id') id: number): void {
         this.boardsService.deleteBoardById(id);
     }
