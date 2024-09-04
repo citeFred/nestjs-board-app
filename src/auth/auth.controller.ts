f837b98c12869daa71b601717f8aafe1f2f2dbc5
citeFred <citefred@gmail.com>
2024-09-04 19:24:19 +0900

    Auth 기능의 ResponseDto 반환 추가, 쿠키 생성은 컨트롤러 계층으로 이동

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,6 +6,8 @@ import { User } from "src/user/user.entity";
 import { SignInRequestDto } from './dto/sign-in-request.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from './get-user.decorator';
+import { UserResponseDto } from './dto/user-response.dto';
+import { ApiResponse } from 'src/common/api-response.dto';
 
 @Controller('api/auth')
 export class AuthController {
@@ -14,24 +16,40 @@ export class AuthController {
     constructor(private authService: AuthService){}
 
     // 회원 가입 기능
-    @Post('/signup') // PostMapping 핸들러 데코레이터
-    signUp(@Body() signUpRequestDto: SignUpRequestDto): Promise<User> {
+    @Post('/signup')
+    async signUp(@Body() signUpRequestDto: SignUpRequestDto): Promise<ApiResponse<UserResponseDto>> {
         this.logger.verbose(`Attempting to sign up user with email: ${signUpRequestDto.email}`);
-        return this.authService.signUp(signUpRequestDto);
+        const user = await this.authService.signUp(signUpRequestDto);
+        const userResponseDto = new UserResponseDto(user);
+        this.logger.verbose(`User signed up successfully: ${JSON.stringify(userResponseDto)}`);
+        return new ApiResponse(true, 201, 'User signed up successfully', userResponseDto);
     }
 
     // 로그인 기능
     @Post('/signin')
-    signIn(@Body() signInRequestDto: SignInRequestDto, @Res() res: Response) {
+    async signIn(@Body() signInRequestDto: SignInRequestDto, @Res() res: Response): Promise<void> {
         this.logger.verbose(`Attempting to sign in user with email: ${signInRequestDto.email}`);
-        return this.authService.signIn(signInRequestDto, res);
+        const { accessToken, user } = await this.authService.signIn(signInRequestDto);
+        const userResponseDto = new UserResponseDto(user);
+        this.logger.verbose(`User signed in successfully: ${JSON.stringify(userResponseDto)}`);
+
+        // [3] 쿠키 설정
+        res.cookie('Authorization', accessToken, {
+            httpOnly: true, // 클라이언트 측 스크립트에서 쿠키 접근 금지
+            secure: false, // HTTPS에서만 쿠키 전송, 임시 비활성화
+            maxAge: 3600000, // 1시간
+            sameSite: 'none', // CSRF 공격 방어
+        });
+
+        res.status(200).json(new ApiResponse(true, 200, 'Sign in successful', { accessToken, user: userResponseDto }));
     }
 
     // 인증된 회원이 들어갈 수 있는 테스트 URL 경로
     @Post('/test')
     @UseGuards(AuthGuard()) // @UseGuards : 핸들러는 지정한 인증 가드가 적용됨 -> AuthGuard()의 'jwt'는 기본값으로 생략가능
-    testForAuth(@GetUser() user: User) {
+    async testForAuth(@GetUser() user: User): Promise<ApiResponse<UserResponseDto>> {
         this.logger.verbose(`Authenticated user accessing test route: ${user.email}`);
-        return { message: 'You are authenticated', user: user };
+        const userResponseDto = new UserResponseDto(user);
+        return new ApiResponse(true, 200, 'You are authenticated', userResponseDto);
     }
 }
