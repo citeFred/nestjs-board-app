2f72b2cd143ba74a5892d5b0d87952470135c27a
citeFred <citefred@gmail.com>
2024-09-05 21:29:53 +0900

    feat : Kakao 로그인 및 회원가입 기능 구현
    * signInWithKakao() : accessToken을 기반으로 한 카카오 회원 정보 활용 로그인+JWT 토큰 발급
    * signUpWithKakao() : 카카오 회원 정보를 활용한 회원 자동 가입
    * getKakaoAccessToken() : AccessToken 발급을 위한 RxJS 요청
    * getKakaoUserInfo() : AccessToken을 통해 회원 정보 요청 위한 RxJS요청

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable, UnauthorizedException, Logger, Res } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from "src/user/user.entity";
 import { Repository } from 'typeorm';
@@ -6,6 +6,9 @@ import { SignUpRequestDto } from './dto/sign-up-request.dto';
 import * as bcrypt from 'bcryptjs';
 import { SignInRequestDto } from './dto/sign-in-request.dto';
 import { JwtService } from '@nestjs/jwt';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class AuthService {
@@ -14,7 +17,8 @@ export class AuthService {
     constructor(
         @InjectRepository(User)
         private usersRepository: Repository<User>,
-        private jwtService: JwtService
+        private jwtService: JwtService,
+        private httpService: HttpService
     ){}
 
     // 회원 가입
@@ -28,7 +32,7 @@ export class AuthService {
         // 비밀번호 해싱
         const hashedPassword = await this.hashPassword(password);
 
-        const user = this.usersRepository.create({
+        const newUser = this.usersRepository.create({
             username,
             password: hashedPassword, // 해싱된 비밀번호 사용
             email,
@@ -38,7 +42,7 @@ export class AuthService {
             detailAddress,
         });
 
-        const savedUser = await this.usersRepository.save(user);
+        const savedUser = await this.usersRepository.save(newUser);
 
         this.logger.verbose(`User signed up successfully with email: ${email}`);
         this.logger.debug(`User details: ${JSON.stringify(savedUser)}`);
@@ -47,7 +51,7 @@ export class AuthService {
     }
 
     // 로그인
-    async signIn(signInRequestDto: SignInRequestDto): Promise<{ accessToken: string, user: User }> {
+    async signIn(signInRequestDto: SignInRequestDto): Promise<{ jwtToken: string, user: User }> {
         const { email, password } = signInRequestDto;
         this.logger.verbose(`Attempting to sign in user with email: ${email}`);
 
@@ -59,18 +63,10 @@ export class AuthService {
                 throw new UnauthorizedException('Incorrect email or password.');
             }
             // [1] JWT 토큰 생성 (Secret + Payload)
-            const payload = { 
-                email: existingUser.email,
-                username: existingUser.username,
-                role: existingUser.role
-             };
-            const accessToken = await this.jwtService.sign(payload);
-            this.logger.verbose(`User signed in successfully with email: ${email}`);
-            this.logger.debug(`Generated JWT Token: ${accessToken}`);
-            this.logger.debug(`User details: ${JSON.stringify(existingUser)}`);
+            const jwtToken = await this.generateJwtToken(existingUser);
 
             // [2] 사용자 정보 반환
-            return { accessToken, user: existingUser };
+            return { jwtToken, user: existingUser };
         } catch (error) {
             this.logger.error('Signin failed', error.stack);
             throw error;
@@ -100,5 +96,94 @@ export class AuthService {
 
         const salt = await bcrypt.genSalt(); // 솔트 생성
         return await bcrypt.hash(password, salt); // 비밀번호 해싱
+    }
+
+    // 카카오 정보 회원 가입
+    async signUpWithKakao(kakaoId: string, profile: any): Promise<User> {
+        const kakaoAccount = profile.kakao_account;
+    
+        const kakaoUsername = kakaoAccount.name;
+        const kakaoEmail = kakaoAccount.email;
+    
+        // 카카오 프로필 데이터를 기반으로 사용자 찾기 또는 생성 로직을 구현
+        const existingUser = await this.usersRepository.findOne({ where: { email: kakaoEmail } });
+        if (existingUser) {
+            return existingUser;
+        }
+
+        // 비밀번호 필드에 랜덤 문자열 생성
+        const temporaryPassword = uuidv4(); // 랜덤 문자열 생성
+        const hashedPassword = await this.hashPassword(temporaryPassword);
+        
+        // 새 사용자 생성 로직
+        const newUser = this.usersRepository.create({
+            username: kakaoUsername,
+            email: kakaoEmail,
+            password: hashedPassword, // 해싱된 임시 비밀번호 사용
+
+            // 기타 필요한 필드 설정
+        });
+        return this.usersRepository.save(newUser);
+    }
+
+    // 카카오 로그인
+    async signInWithKakao(kakaoAuthResCode: string): Promise<{ jwtToken: string, user: User }> {
+        // Authorization Code로 Kakao API에 Access Token 요청
+        const accessToken = await this.getKakaoAccessToken(kakaoAuthResCode);
+
+        // Access Token으로 Kakao 사용자 정보 요청
+        const kakaoUserInfo = await this.getKakaoUserInfo(accessToken);
+
+        // 카카오 사용자 정보를 기반으로 회원가입 또는 로그인 처리
+        const user = await this.signUpWithKakao(kakaoUserInfo.id.toString(), kakaoUserInfo);
+
+        // [1] JWT 토큰 생성 (Secret + Payload)
+        const jwtToken = await this.generateJwtToken(user);
+
+        // [2] 사용자 정보 반환
+        return { jwtToken, user };
+    }
+
+    // Kakao Authorization Code로 Access Token 요청
+    async getKakaoAccessToken(code: string): Promise<string> {
+        const tokenUrl = 'https://kauth.kakao.com/oauth/token';
+        const payload = {
+            grant_type: 'authorization_code',
+            client_id: process.env.KAKAO_CLIENT_ID, // Kakao REST API Key
+            redirect_uri: process.env.KAKAO_REDIRECT_URI,
+            code,
+            client_secret: process.env.KAKAO_CLIENT_SECRET // 필요시 사용
+        };
+    
+        const response = await firstValueFrom(this.httpService.post(tokenUrl, null, {
+            params: payload,
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        }));
+    
+        return response.data.access_token;  // Access Token 반환
+    }
+
+    // Access Token으로 Kakao 사용자 정보 요청
+    async getKakaoUserInfo(accessToken: string): Promise<any> {
+        const userInfoUrl = 'https://kapi.kakao.com/v2/user/me';
+        const response = await firstValueFrom(this.httpService.get(userInfoUrl, {
+            headers: { Authorization: `Bearer ${accessToken}` }
+        }));
+        this.logger.debug(`Kakao User Info: ${JSON.stringify(response.data)}`); // 데이터 확인
+        return response.data;
+    }
+
+    // JWT 생성 공통 메서드
+    async generateJwtToken(user: User): Promise<string> {
+        // [1] JWT 토큰 생성 (Secret + Payload)
+        const payload = { 
+            email: user.email,
+            userId: user.id,
+            role: user.role
+            };
+        const accessToken = await this.jwtService.sign(payload);
+        this.logger.debug(`Generated JWT Token: ${accessToken}`);
+        this.logger.debug(`User details: ${JSON.stringify(user)}`);
+        return accessToken;
     }
 }
