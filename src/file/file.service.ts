49eb3f94be665d8fbb374d4be06faa892da696cf
citeFred <citefred@gmail.com>
2024-09-11 22:48:24 +0900

    파일 업로드 UUID 파일 고유 번호 및 기본 프로필 사진 파일 업로드 로직 추가

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -4,15 +4,20 @@ import * as path from 'path';
 import { File } from './entities/file.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class FileService {
-  private uploadPath = '/Users/inyongkim/Documents/Projects/localStorage';
-
+  private uploadPath = '/Users/inyongkim/Documents/Projects/localStorage/profile';
+  private defaultPath = '/Users/inyongkim/Documents/Projects/localStorage/default'
+  private defaultProfilePicturePath = path.join(this.defaultPath, 'default-profile.png'); // 기본 프로필 사진 경로
+  
   constructor(
-  @InjectRepository(File)
-  private readonly fileRepository: Repository<File>
-  ) { this.ensureUploadPathExists(); }
+    @InjectRepository(File)
+    private readonly fileRepository: Repository<File>
+  ) { 
+    this.ensureUploadPathExists();
+  }
 
   async ensureUploadPathExists() {
     try {
@@ -24,19 +29,46 @@ export class FileService {
 
   // 파일 업로드
   async uploadFile(file: Express.Multer.File) {
-    const filePath = path.join(this.uploadPath, file.originalname);
+    const uniqueFilename = `${uuidv4()}-${file.originalname}`;
+    const filePath = path.join(this.uploadPath, uniqueFilename);
   
     try {
       await fs.writeFile(filePath, file.buffer); // 파일 저장
       return {
         message: 'File uploaded successfully',
-        filePath,
-        filename: file.originalname,
+        filePath: filePath,
+        filename: uniqueFilename,
         mimetype: file.mimetype,
         size: file.size,
       };
     } catch (err) {
       throw new HttpException('Failed to upload file', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  // 기본 프로필 파일 업로드
+  async uploadDefaultProfilePictureFile() {
+    const uniqueFilename = `${uuidv4()}-default-profile.png`;
+    const filePath = path.join(this.uploadPath, uniqueFilename);
+  
+    try {
+      // 기본 프로필 사진을 새 파일로 복사
+      await fs.copyFile(this.defaultProfilePicturePath, filePath);
+      
+      // 파일 메타데이터 가져오기
+      const stats = await fs.stat(filePath);
+
+      const defaultProfilePicture = {
+        filePath: filePath,
+        filename: uniqueFilename,
+        mimetype: 'image/png',
+        size: stats.size,
+      };
+
+      return defaultProfilePicture;
+    } catch (err) {
+      console.error('Error copying default profile picture:', err);
+      throw new HttpException('Failed to upload default profile picture', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
