7bbf1a103d63c81746cf1a678e214c8433e85783
citeFred <citefred@gmail.com>
2024-09-11 13:19:23 +0900

    feat : 파일 엔터티 설계

--- src/file/entities/file.entity.ts
+++ src/file/entities/file.entity.ts
@@ -1,1 +1,25 @@
-export class File {}
+import { Article } from "src/article/article.entity";
+import { BaseEntity } from "src/common/base.entity";
+import { User } from "src/user/user.entity";
+import { Column, Entity, ManyToOne } from "typeorm";
+
+@Entity()
+export class File extends BaseEntity {
+    @Column()
+    filename: string;
+  
+    @Column()
+    mimetype: string;
+  
+    @Column()
+    path: string;
+  
+    @Column()
+    size: number;
+
+    @ManyToOne(() => User, user => user.files, { eager: true })
+    user: User;
+  
+    @ManyToOne(() => Article, article => article.files, { eager: true })
+    article: Article;
+}
