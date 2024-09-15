73f55d0c753ce6fc5723daa1a14a2ca1d300d921
citeFred <citefred@gmail.com>
2024-09-15 18:17:03 +0900

    feat : Attachment Response DTO 생성

---
+++ src/file/attachment/dto/attachment-response.dto.ts
@@ -0,0 +1,26 @@
+import { AttachmentType } from "../entities/attachment-type.enum";
+import { Attachment } from "../entities/attachment.entity";
+
+export class AttachmentResponseDto {
+    id: number;
+    filename: string;
+    mimetype: string;
+    path: string;
+    size: number;
+    attachmentType: AttachmentType;
+    url: string;
+    createdAt: Date;
+    updatedAt: Date;
+
+    constructor(file: Attachment){
+        this.id = file.id;
+        this.filename = file.filename;
+        this.mimetype = file.mimetype;
+        this.path = file.path;
+        this.size = file.size;
+        this.attachmentType = file.attachmentType;
+        this.url = file.url;
+        this.createdAt = file.createdAt;
+        this.updatedAt = file.updatedAt;
+    }
+}
