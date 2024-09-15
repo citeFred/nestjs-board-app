ce0e39fe01a2328f021c8beee4c6a1ff116a36f4
citeFred <citefred@gmail.com>
2024-09-15 18:15:46 +0900

    feat : 게시글 첨부 파일 Attachment 엔터티 생성

---
+++ src/file/attachment/entities/attachment.entity.ts
@@ -0,0 +1,28 @@
+import { BaseEntity } from "src/common/base.entity";
+import { Column, Entity, ManyToOne } from "typeorm";
+import { Article } from "src/article/article.entity";
+import { AttachmentType } from "./attachment-type.enum";
+
+@Entity()
+export class Attachment extends BaseEntity {
+    @Column()
+    filename: string;
+  
+    @Column()
+    mimetype: string;
+  
+    @Column()
+    path: string;
+  
+    @Column()
+    size: number;
+
+    @Column()
+    attachmentType: AttachmentType;
+
+    @Column()
+    url: string;
+
+    @ManyToOne(() => Article, article => article.attachments, { eager: false })
+    article: Article;
+}
