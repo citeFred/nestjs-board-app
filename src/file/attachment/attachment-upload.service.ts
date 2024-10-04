297dfdede2a3790561c6bb3eccb9afc6b1b0f1ff
citeFred <citefred@gmail.com>
2024-10-04 18:19:22 +0900

    refactor : 첨부파일 및 프로필 사진 업로드 서비스 로컬 -> S3로 변경

--- src/file/attachment/attachment-upload.service.ts
+++ src/file/attachment/attachment-upload.service.ts
@@ -1,44 +1,24 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
-import { promises as fs } from 'fs';
-import * as path from 'path';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
 import { Attachment } from './entities/attachment.entity';
+import { S3Service } from '../aws-s3/s3.service';
 
 @Injectable()
 export class AttachmentUploadService {
-  private uploadPath = path.join(process.cwd(), 'public', 'uploads', 'attachment'); 
-  
   constructor(
     @InjectRepository(Attachment)
-    private readonly attachmentRepository: Repository<Attachment>
-  ) { 
-    this.ensureUploadPathExists();
-  }
-
-  async ensureUploadPathExists() {
-    try {
-      await fs.mkdir(this.uploadPath, { recursive: true });
-    } catch (err) {
-      throw new HttpException('Failed to create upload directory', HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-  }
+    private readonly attachmentRepository: Repository<Attachment>,
+    private readonly s3Service: S3Service,
+  ) {}
 
   // 파일 업로드
   async uploadFile(file: Express.Multer.File) {
-    const uniqueFilename = `${uuidv4()}-${file.originalname}`;
-    const filePath = path.join(this.uploadPath, uniqueFilename);
-    const fileUrl = `http://localhost:${process.env.SERVER_PORT}/files/uploads/attachment/${uniqueFilename}`;
-
     try {
-      await fs.writeFile(filePath, file.buffer); // 파일 저장
+      const fileUrl = await this.s3Service.uploadFile(file, 'your-bucket-name');
       return {
         message: 'File uploaded successfully',
-        filePath: filePath,
-        filename: uniqueFilename,
-        mimetype: file.mimetype,
-        size: file.size,
         url: fileUrl,
       };
     } catch (err) {
