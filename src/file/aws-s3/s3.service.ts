77f357503131c63fefc237e9b4924d9d3ae81721
citeFred <citefred@gmail.com>
2024-10-07 20:07:48 +0900

    fix : AWS S3 파일 업로드시 한글 인코딩 문제를 노멀라이즈로 변경, 기타 경로를 환경 변수로 처리

--- src/file/aws-s3/s3.service.ts
+++ src/file/aws-s3/s3.service.ts
@@ -8,7 +8,7 @@ export class S3Service {
 
   constructor() {
     this.s3 = new S3({
-      region: 'ap-northeast-2',
+      region: process.env.AWS_S3_REGION,
       credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
@@ -16,17 +16,32 @@ export class S3Service {
     });
   }
 
-  async uploadFile(file: Express.Multer.File, bucketName: string): Promise<string> {
-    const encodedFileName = encodeURIComponent(`${Date.now()}-${file.originalname}`);
+  async uploadFile(file: Express.Multer.File, prefix: string): Promise<any> {
+    const normalizeFileName = this.normalizeFileName(file.originalname);
+    const filePath = `${prefix}/${normalizeFileName}`;
 
     const command = new PutObjectCommand({
-      Bucket: bucketName,
-      Key: encodedFileName,
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: filePath,
       Body: file.buffer,
       ContentType: file.mimetype,
     });
-  
+
     await this.s3.send(command);
-    return `https://${bucketName}.s3.amazonaws.com/${encodedFileName}`; // 반환 URL
+
+    // CloudFront URL 반환 및 메타데이터
+    return {
+      url: `https://${process.env.AWS_CLOUDFRONT_DOMAIN}/${filePath}`,
+      filename: normalizeFileName,
+      mimetype: file.mimetype,
+      size: file.size,
+    };
+  }
+
+  private normalizeFileName(fileName: string): string {
+    return fileName
+      .replace(/[^a-zA-Z0-9.-]/g, '_')  // 알파벳, 숫자, 점, 하이픈만 허용
+      .replace(/_{2,}/g, '_')            // 연속된 언더바를 하나로
+      .replace(/^_|_$/g, '');            // 언더바로 시작하거나 끝나는 경우 제거
   }
 }
