9571a006e94e20edbfc8fd569b95279efd82c8d0
citeFred <citefred@gmail.com>
2024-10-23 14:23:52 +0900

    chore : HTTPS 요청을 받기 위한 백엔드 API 서버 설정

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { Logger } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as path from 'path';
+import * as fs from 'fs';
+import * as https from 'https'; // https 모듈 추가
 
 dotenv.config();
 
@@ -12,7 +14,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // cookie parser 미들웨어 추가
   app.use(cookieParser());
-  
+
   app.enableCors({
     origin: [
       'http://localhost:8100','http://localhost:4200', // 로컬 개발용
@@ -26,12 +28,27 @@ async function bootstrap() {
     exposedHeaders: ['Authorization'], // 이 부분 추가
   });
 
-
   app.useStaticAssets(path.join(process.cwd(), 'public'), {
     prefix: '/files/'
   });
-  
-  await app.listen(process.env.SERVER_PORT);
-  Logger.log(`Application Running on Port : ${process.env.SERVER_PORT}`)
+
+  // HTTPS 옵션 설정
+  const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/api.boardapp.site/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/api.boardapp.site/fullchain.pem'),
+  };
+
+  // HTTPS 서버 생성
+  const server = https.createServer(httpsOptions, app.getHttpAdapter().getInstance());
+
+  // NestJS 애플리케이션을 HTTPS 서버에 연결 (HTTPS 적용)
+  server.listen(process.env.HTTPS_SERVER_PORT, ()=> {
+    Logger.log(`Application Running on [SECURED] https://api.boardapp.site:${process.env.HTTPS_SERVER_PORT}`);
+  });
+
+  // NestJS 애플리케이션 시작 (HTTPS 기존)
+  await app.listen(process.env.HTTP_SERVER_PORT, ()=> {
+    Logger.log(`Application Running on [BASIC] http://localhost:${process.env.HTTP_SERVER_PORT}`);
+  });
 }
 bootstrap();
